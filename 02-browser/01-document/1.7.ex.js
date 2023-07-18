d0f903e528baca06fa8ba2448bca4a9a73e18eae
Daekeun Ko <kdkcom1234@gmail.com>
2023-07-18 18:18:35 +0900

    sort

--- 02-browser/01-document/1.7.ex.js
+++ 02-browser/01-document/1.7.ex.js
@@ -1,6 +1,7 @@
 const rows =
   document.querySelectorAll("tbody > tr");
 
+// sort를 쓸려면 배열로 변환.
 const sortedRows = Array.from(rows).sort((a, b) =>
   a.children[0].textContent.localeCompare(
     b.children[0].textContent
@@ -9,43 +10,4 @@ const sortedRows = Array.from(rows).sort((a, b) =>
 
 for (let row of sortedRows) {
   document.querySelector("tbody").append(row);
-}
-
-let container =
-  document.getElementById("container");
-let data = {
-  Fish: {
-    trout: {},
-    salmon: {},
-  },
-
-  Tree: {
-    Huge: {
-      sequoia: {},
-      oak: {},
-    },
-    Flowering: {
-      "apple tree": {},
-      magnolia: {},
-    },
-  },
-};
-
-createTree(container, data);
-
-function createTree(container, data) {
-  // 객체에 속성이 있으면 처리함
-  // 재귀 함수 탈출 조건
-  if (Object.keys(data).length > 0) {
-    const ul = document.createElement("ul");
-
-    for (let prop in data) {
-      const li = document.createElement("li");
-      li.textContent = prop;
-      ul.append(li);
-      // 하위 데이터에 데이터 다시 트리 생성
-      createTree(li, data[prop]);
-    }
-    container.append(ul);
-  }
 }
