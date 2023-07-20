dc70f8fd4831fb44c76c9f0429f8c88134d5d0e2
Daekeun Ko <kdkcom1234@gmail.com>
2023-07-20 16:12:36 +0900

    todo 키보드 엔터

--- todo/todo.js
+++ todo/todo.js
@@ -2,6 +2,7 @@
 // 입력창에 메모를 입력하고
 // 추가버튼을 클릭하면 목록의 첫번째에 추가
 // + 입력창에 값이 초기화
+// + 입력창에 엔터를 눌러도 추가
 
 // 2번째 use-case
 // 목록에서 항목을 클릭하면
@@ -19,27 +20,37 @@ console.log(container);
 // 버튼을 클릭하면 작동할 함수를 등록
 container
   .querySelector("button")
-  .addEventListener("click", () => {
-    const input =
-      container.querySelector("input");
-    // 입력값을 가져옴
-    const value = input.value;
-    console.log(value);
+  .addEventListener("click", addItem);
 
-    // 아이템(li)을 생성하고, 입력값을 컨텐트로 넣음
-    const item = document.createElement("li");
-    item.textContent = value;
-    console.log(item);
-
-    // 2번째 use-case 구현
-    // 항목을 클릭하면 삭제하는 이벤트 핸들러 추가
-    item.addEventListener("click", () => {
-      item.remove();
-    });
-
-    // 아이템을 목록에 추가
-    container.querySelector("ul").prepend(item);
-
-    // +입력값 초기화
-    input.value = "";
+// 입력창에서 키를 눌렀다가 뗐을 때 작동할 함수를 등록
+container
+  .querySelector("input")
+  .addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      addItem();
+    }
   });
+
+function addItem() {
+  const input = container.querySelector("input");
+  // 입력값을 가져옴
+  const value = input.value;
+  console.log(value);
+
+  // 아이템(li)을 생성하고, 입력값을 컨텐트로 넣음
+  const item = document.createElement("li");
+  item.textContent = value;
+  console.log(item);
+
+  // 2번째 use-case 구현
+  // 항목을 클릭하면 삭제하는 이벤트 핸들러 추가
+  item.addEventListener("click", () => {
+    item.remove();
+  });
+
+  // 아이템을 목록에 추가
+  container.querySelector("ul").prepend(item);
+
+  // +입력값 초기화
+  input.value = "";
+}
