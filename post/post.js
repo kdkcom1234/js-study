c60e2551a28403705582fbb8cc815fa510947706
Daekeun Ko <kdkcom1234@gmail.com>
2023-07-26 13:06:00 +0900

    post 목록 조회

--- post/post.js
+++ post/post.js
@@ -24,3 +24,41 @@
 //    | ------------- |
 //    | 생성시간       |
 //    -----------------
+
+(async () => {
+  const body = document.body;
+  const url = "http://localhost:8080/posts";
+
+  // 1. fetch, 서버에서 데이터 가져오기
+  const response = await fetch(url);
+  const result = await response.json();
+  console.log(result);
+
+  // 배열 메서드를 사용하기 위해서...
+  const data = Array.from(result);
+  console.log(data);
+
+  // 2.-- 데이터배열 반복문으로 html문자열 만들고,
+  // 컨테이너에 추가
+  data.forEach((item) => {
+    const template = /*html*/ `
+      <div style="width:300px; margin-bottom:2rem;" data-no="${
+        item.no
+      }">
+        <em>${item.creatorName}</em>
+        <hr>
+        <h3>${item.title}</h3>
+        <p>${item.content}</p>
+        <hr>
+        <small>${new Date(
+          item.createdTime
+        ).toLocaleString()}</small>
+      </div>
+    `;
+
+    body.insertAdjacentHTML(
+      "beforeend",
+      template
+    );
+  });
+})();
