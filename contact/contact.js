dd487e05304ca5c5fb9595bcadb356eb3977bcd8
Daekeun Ko <kdkcom1234@gmail.com>
2023-08-07 12:23:17 +0900

    authorization 헤더 추가

--- contact/contact.js
+++ contact/contact.js
@@ -36,7 +36,19 @@ async function getPagedList(page, query) {
     url = `http://localhost:8080/contacts/paging?page=${page}&size=${PAGE_SIZE}`;
   }
 
-  const response = await fetch(url);
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${getCookie(
+        "token"
+      )}`,
+    },
+  });
+  // 401: 미인증, 403: 미인가(허가없는)
+  if ([401, 403].includes(response.status)) {
+    // 로그인 페이지로 튕김
+    alert("인증처리가 되지 않았습니다.");
+    window.location.href = "/login.html";
+  }
   // 결과가 배열
   const result = await response.json();
   console.log(result);
@@ -307,6 +319,11 @@ function setBtnActive() {
       `http://localhost:8080/contacts/${email.value}`,
       {
         method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${getCookie(
+            "token"
+          )}`,
+        },
       }
     );
 
@@ -391,6 +408,9 @@ function setBtnActive() {
               headers: {
                 "content-type":
                   "application/json",
+                Authorization: `Bearer ${getCookie(
+                  "token"
+                )}`,
               },
               body: JSON.stringify({
                 name,
